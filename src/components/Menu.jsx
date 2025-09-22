77ef63f2230875ff1a67df6700f8bb8aa49aa18a
Sajith Minrutha <minruthasajith@gmail.com>
2025-09-22 19:19:12 +0530

    add menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+function Menu() {
+  return (
+    <div>
+      <div>
+        <a href="">Dashboard</a>
+        <a href="">Maths</a>
+        <a href="">Physics</a>
+        <a href="">Chemistry</a>
+        <a href="">ToDo</a>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
